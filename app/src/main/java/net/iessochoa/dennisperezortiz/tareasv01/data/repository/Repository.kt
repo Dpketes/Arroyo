2232608c8ead2cfc3ea79343ec6d43ebbcf3b9ff
dpketes <dennisperezz2003@gmail.com>
2024-12-12 19:40:59 +0100

    Repository

--- app/src/main/java/net/iessochoa/dennisperezortiz/tareasv01/data/repository/Repository.kt
+++ app/src/main/java/net/iessochoa/dennisperezortiz/tareasv01/data/repository/Repository.kt
@@ -1,16 +1,25 @@
 package net.iessochoa.dennisperezortiz.tareasv01.data.repository
 
+import net.iessochoa.dennisperezortiz.tareasv01.TareasApplication
+import net.iessochoa.dennisperezortiz.tareasv01.data.db.dao.TareasDao
+import net.iessochoa.dennisperezortiz.tareasv01.data.db.database.TareasDataBase
 import net.iessochoa.dennisperezortiz.tareasv01.data.db.entities.Tarea
-import net.iessochoa.dennisperezortiz.tareasv01.data.tempmodel.TempModelTareas
 
 object Repository {
+    //modelo de datos
+    private lateinit var modelTareas: TareasDao
     //inicio del objeto singleton
     operator fun invoke() {
-        //iniciamos el modelo
-        TempModelTareas()
+
+        //iniciamos el modelo con la base de datos
+        modelTareas = TareasDataBase
+            .getDatabase(TareasApplication.application.applicationContext)
+            .tareasDao()
     }
-    fun addTarea(tarea: Tarea)= TempModelTareas.addTarea(tarea)
-    fun delTarea(tarea: Tarea)= TempModelTareas.delTarea(tarea)
-    fun getAllTareas()= TempModelTareas.getAllTareas()
-    fun getTarea(id: Long) = TempModelTareas.getTarea(id)
+    //Métodos CRUD a la base de datos
+    suspend fun addTarea(tarea: Tarea)= modelTareas.addTarea(tarea)
+    suspend fun delTarea(tarea: Tarea)= modelTareas.delTarea(tarea)
+    suspend fun getTarea(id:Long)= modelTareas.getTarea(id)
+    fun getAllTareas()= modelTareas.getTareas()
 }
+
